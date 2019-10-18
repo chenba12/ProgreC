0affa2b488a38d1aca8f9d87ed3e25b82ae32e8d
Chen <chen24201@gmail.com>
2019-10-18 12:59:38 +0300

    beans

---
+++ src/main/java/com/progresee/app/beans/Exercise.java
@@ -0,0 +1,11 @@
+package com.progresee.app.beans;
+
+import com.google.cloud.firestore.FieldValue;
+
+public class Exercise {
+	
+	private String uid;
+	private String exerciseTitle;
+	private FieldValue dateCreated;
+
+}
---
+++ src/main/java/com/progresee/app/beans/User.java
@@ -0,0 +1,23 @@
+package com.progresee.app.beans;
+
+import com.google.cloud.firestore.FieldValue;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	
+	private String uid;
+	private String email;
+	private String fullName;
+	private String profileUrl;
+	private String provider;
+	private FieldValue dateCreated;
+	private FieldValue signedIn;
+	
+
+}
