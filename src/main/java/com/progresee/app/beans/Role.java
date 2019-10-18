bb196bc519b61ef9b0c623ddef10cea58b22fac7
Chen <chen24201@gmail.com>
2019-10-18 12:50:23 +0300

    bye bye

--- src/main/java/com/progresee/app/beans/Role.java
+++
@@ -1,7 +0,0 @@
-package com.progresee.app.beans;
-
-public enum Role {
-	
-	ROLE_USER,ROLE_ADMIN
-
-}
--- src/main/java/com/progresee/app/beans/UserLoginType.java
+++
@@ -1,6 +0,0 @@
-package com.progresee.app.beans;
-
-public enum UserLoginType {
-
-	GOOGLE,FACEBOOK
-}
